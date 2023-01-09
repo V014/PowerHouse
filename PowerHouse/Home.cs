c6404b51f1506af2e30e08d026b3d405e54473ad
Wanga Kanjala <wangakanjala@gmail.com>
2023-01-10 01:31:13 +0200

    Reordered the file locations

---
+++ PowerHouse/Home.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerHouse
+{
+    public partial class Home : MetroFramework.Forms.MetroForm
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            float fcpu = pCPU.NextValue();
+            float fram = pRAM.NextValue();
+            bunifuProgressBarCPU.Value = (int)fcpu;
+            bunifuProgressBarRAM.Value = (int)fram;
+            lblCPU.Text = string.Format("{0:0.00}%", fcpu);
+            lblRAM.Text = string.Format("{0:0.00}%", fram);
+            chart.Series["CPU"].Points.AddY(fcpu);
+            chart.Series["RAM"].Points.AddY(fram);
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void btnGraph_Click(object sender, EventArgs e)
+        {
+            this.ClientSize = new System.Drawing.Size(569, 404);
+        }
+
+        private void bunifuLabel1_Click(object sender, EventArgs e)
+        {
+            this.ClientSize = new System.Drawing.Size(569, 203);
+        }
+    }
+}
